aeb921d875aaeda748b0c7a8fc7905d61f600c0f
Jascha Ehrenreich <jascha@jaeh.at>
2014-12-21 20:21:39 +0100

    disabled for now

--- db.js
+++ db.js
@@ -1,24 +1,10 @@
 'use strict';
 var path     = require('path')
-  , tungus   = require('tungus')
-  , mongoose = require('mongoose')
   , log      = require('magic-log')
   , Schema   = mongoose.Schema
 ;
 
 module.exports = function init (settings, next) {
-  if ( ! next ) {
-    return log.error('magic-db', 'called without next callback, this breaks the middleware flow.');
-  }
-
-  if ( ! settings.database) {
-    log.error('magic-db', 'called without a valid settings object.', settings);
-    return next('magic-db init called without a valid settings object');
-  }
-
-  mongoose.connect(settings.database, function (err) {
-    //log('mongoose connected');
-    next(err, mongoose);
-  });
+  next();
 }
 
