0d5944382e7c105019f32741efed61a89eff5225
Jascha Ehrenreich <jascha@jaeh.at>
2015-01-16 22:24:45 +0100

    added mongoose

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 'use strict';
 var path     = require('path')
   , log      = require('magic-log')
+  , mongoose = require('mongoose')
 ;
 
 module.exports = function init (settings, next) {
-  next();
+  log('magic-db', 'connecting to db');
+  mongoose.connect('mongodb://localhost/' + settings.name, next);
 }
-
